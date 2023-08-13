a906eb5bab3d87c67c779f11174d21498a777305
Immanuel Baur <immanuel.baur@mercedes-benz.com>
2023-08-13 13:41:07 -0700

    REST API exercise

--- lesson-3-implementing-message-passing/flask-starter/app.py
+++ lesson-3-implementing-message-passing/flask-starter/app.py
@@ -10,5 +10,21 @@ app = Flask(__name__)
 def health():
     return jsonify({'response': 'Hello World!'})
 
+@app.route('/api/orders/computers', methods=['POST'])
+def create_computer_order():
+    # Get request body
+    data = request.json
+    # Create order
+    order = create_order(data)
+    # Return response
+    return jsonify(order)
+
+@app.route('/api/orders/computers', methods=['GET'])
+def get_computer_orders():
+    # Retrieve orders
+    orders = retrieve_orders()
+    # Return response
+    return jsonify(orders)
+
 if __name__ == '__main__':
     app.run()
