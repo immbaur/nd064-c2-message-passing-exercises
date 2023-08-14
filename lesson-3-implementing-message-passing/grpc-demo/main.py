9a202e2ffe182c18537614cd41ff0cc443fe8417
Immanuel Baur <immanuel.baur@mercedes-benz.com>
2023-08-13 17:37:20 -0700

    gRPC exercise

--- lesson-3-implementing-message-passing/grpc-demo/main.py
+++ lesson-3-implementing-message-passing/grpc-demo/main.py
@@ -2,28 +2,66 @@ import time
 from concurrent import futures
 
 import grpc
-import item_pb2
-import item_pb2_grpc
+# import item_pb2
+# import item_pb2_grpc
+
+import order_pb2
+import order_pb2_grpc
 
 
-class ItemServicer(item_pb2_grpc.ItemServiceServicer):
+class OrderServicer(order_pb2_grpc.OrderServiceServicer):
     def Create(self, request, context):
 
-        request_value = {
-            "name": request.name,
-            "brand_name": request.brand_name,
-            "id": int(request.id),
-            "weight": request.weight,
+        Order = {
+            "id": request.id,
+            "created_by": request.created_by,
+            "status": order_pb2.OrderMessage.Status.Name(request.status),
+            "created_at": request.created_at,
+            "equipment": [order_pb2.OrderMessage.Equipment.Name(equipment) for equipment in request.equipment],
         }
-        print(request_value)
 
-        return item_pb2.ItemMessage(**request_value)
+        print(Order)
+
+        return order_pb2.OrderMessage(**Order)
+    
+    def Get(self, request, context):
+        first_order = order_pb2.OrderMessage(
+            id="2222",
+            created_by="USER123",
+            status=order_pb2.OrderMessage.Status.QUEUED,
+            created_at='2020-03-12',
+            equipment=[order_pb2.OrderMessage.Equipment.KEYBOARD]
+        )
+        second_order = order_pb2.OrderMessage(
+            id="3333",
+            created_by="USER123",
+            status=order_pb2.OrderMessage.Status.QUEUED,
+            created_at='2020-03-11',
+            equipment=[order_pb2.OrderMessage.Equipment.MOUSE]
+        )
+        result = order_pb2.OrderMessageList()
+        result.orders.extend([first_order, second_order])
+       
+        return result
+
+# class ItemServicer(item_pb2_grpc.ItemServiceServicer):
+#     def Create(self, request, context):
+
+#         request_value = {
+#             "name": request.name,
+#             "brand_name": request.brand_name,
+#             "id": int(request.id),
+#             "weight": request.weight,
+#         }
+#         print(request_value)
+
+#         return item_pb2.ItemMessage(**request_value)
 
 
 # Initialize gRPC server
 server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
-item_pb2_grpc.add_ItemServiceServicer_to_server(ItemServicer(), server)
-
+# item_pb2_grpc.add_ItemServiceServicer_to_server(ItemServicer(), server)
+order_pb2_grpc.add_OrderServiceServicer_to_server(OrderServicer(),server)
 
 print("Server starting on port 5005...")
 server.add_insecure_port("[::]:5005")
--- lesson-3-implementing-message-passing/grpc-demo/writer.py
+++ lesson-3-implementing-message-passing/grpc-demo/writer.py
@@ -2,6 +2,9 @@ import grpc
 import item_pb2
 import item_pb2_grpc
 
+import order_pb2
+import order_pb2_grpc
+
 """
 Sample implementation of a writer that can be used to write messages to gRPC.
 """
@@ -9,15 +12,31 @@ Sample implementation of a writer that can be used to write messages to gRPC.
 print("Sending sample payload...")
 
 channel = grpc.insecure_channel("localhost:5005")
-stub = item_pb2_grpc.ItemServiceStub(channel)
+# stub = item_pb2_grpc.ItemServiceStub(channel)
+stub2 = order_pb2_grpc.OrderServiceStub(channel)
 
 # Update this with desired payload
-item = item_pb2.ItemMessage(
-    name="Non-Stick Frying Pan",
-    brand_name="Chef Dreams",
-    id=4,
-    weight=4.5
+# item = item_pb2.ItemMessage(
+#     name="Non-Stick Frying Pan",
+#     brand_name="Chef Dreams",
+#     id=4,
+#     weight=4.5
+# )
+
+# create random OrderMessage
+order = order_pb2.OrderMessage(
+    id="1",
+    created_by="user1",
+    status=order_pb2.OrderMessage.Status.QUEUED,
+    created_at="2020-01-01",
+    equipment=[order_pb2.OrderMessage.Equipment.KEYBOARD, order_pb2.OrderMessage.Equipment.MOUSE]
 )
 
+# response = stub.Create(item)
+# print(response)
 
-response = stub.Create(item)
+response2 = stub2.Create(order)
+print(response2)
+
+response3 = stub2.Get(order_pb2.Empty())
+print(response3)
